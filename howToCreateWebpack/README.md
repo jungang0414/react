7717281db69f8ccae1c3a3a38a9edfc5c6e99ba0
jungang0414 <dayr88520@gmail.com>
2024-04-19 16:22:17 +0800

    新增webpack.config.js檔案並調整打包設定

--- howToCreateWebpack/README.md
+++ howToCreateWebpack/README.md
@@ -18,7 +18,7 @@ npm i webpack webpack-cli webpack-dev-server --save-dev
 npm i babel-loader @babel/core @babel/preset-env @babel/runtime @babel/plugin-transform-runtime @babel/preset-react --save-dev
 ```
 
-#### 安裝HTML&CSS loader(.html/.css檔案)
+#### 安裝 HTML&CSS loader(.html/.css檔案)
 ```
 npm i html-webpack-plugin css-loader style-loader postcss-loader postcss postcss-preset-env --save-dev
 ```
@@ -26,4 +26,8 @@ npm i html-webpack-plugin css-loader style-loader postcss-loader postcss postcss
 #### 安裝 React & tailwind
 ```
 npm i react react-dom tailwindcss --save
+```
+
+#### 在根目錄底下設定webpack.config.js檔案, 此處用來調整打包設定.
+```
 ```
---
+++ howToCreateWebpack/webpack.config.js
@@ -0,0 +1,47 @@
+//這裡是webpack的設定檔，webpack會依照這個檔案的設定來進行打包
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+//這裡是設定webpack的設定
+module.exports = {
+    entry: "./src/index.js", //進入點
+    output: {
+        publicPath: "http://localhost:3000/", //這裡是設定webpack-dev-server的位置
+    },
+    resolve: {
+        extensions: ["js", "jsx"], //這裡是設定import時可以省略副檔名
+    },
+
+    devServer: {
+        port: 3000, //這裡是設定webpack-dev-server的port
+        historyApiFallback: true, //這裡是設定webpack-dev-server的historyApiFallback
+    },
+    //這裡是設定webpack要使用的loader
+    module: {
+        rules: [
+            {
+                test: /\.m?js/, //這裡是設定webpack要處理的檔案格式
+                type: "javascript/auto",
+                resolve: {
+                    fullySpecified: false,
+                },
+            },
+            {
+                test: /\.(css|s[ac]ss)$/i, 
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            },
+            {
+                test: /\.(ts|tsx|js|jsx)$/, 
+                exclude: /node_modules/, //這裡是設定webpack要排除的檔案
+                use: {
+                    loader: "babel-loader", //這裡是設定webpack要使用的loader
+                },
+            },
+        ],
+    },
+    //這裡是設定webpack要使用的plugin
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+};
